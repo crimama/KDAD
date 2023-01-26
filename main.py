8b3909ea9b050469c5f424560ad8938454e59e59
crimama2 <crimama2@gmail.com>
2023-01-26 17:58:29 +0000

    remove loss_concat method

--- main.py
+++ main.py
@@ -36,21 +36,7 @@ def loss_function(a, b):
                                       b[item].view(b[item].shape[0],-1)))
     return loss
 
-def loss_concat(a, b):
-    mse_loss = torch.nn.MSELoss()
-    cos_loss = torch.nn.CosineSimilarity()
-    loss = 0
-    a_map = []
-    b_map = []
-    size = a[0].shape[-1]
-    for item in range(len(a)):
-        #loss += mse_loss(a[item], b[item])
-        a_map.append(F.interpolate(a[item], size=size, mode='bilinear', align_corners=True))
-        b_map.append(F.interpolate(b[item], size=size, mode='bilinear', align_corners=True))
-    a_map = torch.cat(a_map,1)
-    b_map = torch.cat(b_map,1)
-    loss += torch.mean(1-cos_loss(a_map,b_map))
-    return 
+
 
 def run(cfg):
     setup_seed(cfg['TRAIN']['seed'])
