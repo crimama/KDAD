7d5e1024cc0ec8c87b11dc88b2596ed351b32fc4
crimama2 <crimama2@gmail.com>
2023-01-26 18:49:53 +0000

    update log update time tracking edit how to build encoder so that we can change easily using yaml

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.dataset import CustomDataset
-from src.models.resnet import resnet18, resnet34, resnet50, wide_resnet50_2
-from src.models.de_resnet import de_resnet18, de_resnet34, de_wide_resnet50_2, de_resnet50
+
+from src.models import resnet,de_resnet 
 from src.test import evaluation
 from src.augmentation import get_data_transforms
+from src.log import setup_default_logging
 
 from torch.utils.data import DataLoader,DataLoader2
 import torch.nn as nn 
@@ -11,12 +12,14 @@ import torch
 import torchvision.transforms as transforms 
 
 import yaml 
-from tqdm.auto import tqdm 
+import time 
 import numpy as np 
 import random 
 import os 
 import argparse 
+import logging 
 
+_logger = logging.getLogger('train') 
 
 def setup_seed(seed):
     torch.manual_seed(seed)
@@ -38,13 +41,38 @@ def loss_function(a, b):
                                       b[item].view(b[item].shape[0],-1)))
     return loss
 
+class AverageMeter:
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
 
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 def run(cfg):
+    
+    # setting savedir and seed 
     setup_seed(cfg['TRAIN']['seed'])
     device = cfg['TRAIN']['device']
     
+    # set logger 
+    setup_default_logging(log_path=os.path.join(cfg['SAVE']['savedir'],'log.txt'))
+    _logger.info('Device: {}'.format(device))
+    
+    
+    #build augmentation 
     data_transform, gt_transform = get_data_transforms(cfg['DATA']['imgsize'],cfg['DATA']['imgsize'])
+    
+    #build datasets and dataloader 
     trainloader = DataLoader(
                     dataset = CustomDataset(
                                             root          = cfg['DATA']['datadir'],
@@ -71,17 +99,31 @@ def run(cfg):
                     batch_size = 1,
                     shuffle    = False
                     )
-
-    encoder,bn = resnet34(pretrained=True)
+    
+    
+    # build encoder,bn and decoder 
+    #encoder,bn = resnet34(pretrained=True)
+    encoder,bn = __import__('src').models.resnet.__dict__[f"{cfg['MODEL']['encoder']}"](pretrained=True)
     encoder,bn = encoder.to(device), bn.to(device)
     encoder.eval()
-    decoder = de_resnet34(pretrained=False)
+    #decoder = de_resnet34(pretrained=False)
+    decoder = __import__('src').models.de_resnet.__dict__[f"{cfg['MODEL']['decoder']}"]()
     decoder = decoder.to(device)
+    
+    # set optimizer 
     optimizer = torch.optim.Adam(list(decoder.parameters()) + list(bn.parameters()),lr = cfg['TRAIN']['lr'],betas=(0.5,0.999))
-    print("All loaded, train start")
+    _logger.info("All loaded, train start")
     train(trainloader,testloader,encoder,bn,decoder,optimizer,cfg)
     
 def train(trainloader,testloader,encoder,bn,decoder,optimizer,cfg):
+    # set AverageMeter 
+    batch_time_m = AverageMeter() # 배치 1개 도는 시간  
+    data_time_m = AverageMeter() #데이터 뽑는 시간 
+    epoch_time_m = AverageMeter()
+    
+    batch_end = time.time() 
+    epoch_end = time.time()
+    
     best = 0 
     device = cfg['TRAIN']['device']
     for epoch in range(cfg['TRAIN']['epochs']):
@@ -89,25 +131,44 @@ def train(trainloader,testloader,encoder,bn,decoder,optimizer,cfg):
         decoder.train()
         loss_list = [] 
         for batch_imgs,_,batch_labels in trainloader:
+            data_time_m.update(time.time()-batch_end)
+            
             batch_imgs = batch_imgs.to(device)
+            
+            # predict 
             inputs = encoder(batch_imgs)
             outputs = decoder(bn(inputs))
+            
             loss = loss_function(inputs,outputs)
             
+            # loss update
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             loss_list.append(loss.item())
-        print('epoch [{}/{}], loss:{:.4f}'.format(epoch + 1,cfg['TRAIN']['epochs'] , np.mean(loss_list)))
+            
+            batch_time_m.update(time.time()-batch_end)
+            batch_end = time.time()
+            
+        epoch_time_m.update(time.time()-epoch_end) 
+        epoch_end = time.time()  
+        #logging 
+        _logger.info(f"epoch: [{epoch+1}/{cfg['TRAIN']['epochs']}] "
+                     f"loss: {np.mean(loss_list):.4f} "
+                     f"lr = {optimizer.param_groups[0]['lr']:.4f} "
+                     f"batch_time = {batch_time_m.val:.4f} "
+                     f"epoch_time = {epoch_time_m.val:.4f} "
+                     )
         
         if (epoch +1) % 2 == 0:
             auroc_px, auroc_sp, aupro_px = evaluation(encoder, bn, decoder, testloader, device)
-            print('Pixel Auroc:{:.3f} | Sample Auroc:{:.3f} | Pixel Aupro:{:.3}'.format(auroc_px, auroc_sp, aupro_px))
+            _logger.info(f'Pixel Auroc:{auroc_px:.3f} | Sample Auroc:{auroc_sp:.3f} | '
+                         'Pixel Aupro:{auroc_px:.3}')
             
             if auroc_px > best:
                 torch.save(decoder,os.path.join(cfg['SAVE']['savedir'],'best_decoder.pt'))
                 torch.save(bn,os.path.join(cfg['SAVE']['savedir'],'best_bn.pt'))
-                
+                _logger.info(f"New Best model saved - Epoch : {epoch+1}")
                 best = auroc_px
             
     torch.save(decoder,os.path.join(cfg['SAVE']['savedir'],'last_decoder.pt'))
